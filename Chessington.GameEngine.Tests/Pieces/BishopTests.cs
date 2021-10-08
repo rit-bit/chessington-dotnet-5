5ecf224337edc09eb0ac487093b72ab8e01c72fa
Richard Sutch <richard.sutch@softwire.com>
2021-10-08 14:20:29 +0100

    Added test to check the bishop can move on 1 diagonal

--- Chessington.GameEngine.Tests/Pieces/BishopTests.cs
+++ Chessington.GameEngine.Tests/Pieces/BishopTests.cs
@@ -1,9 +1,26 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using Chessington.GameEngine.Pieces;
+using FluentAssertions;
+using NUnit.Framework;
 
 namespace Chessington.GameEngine.Tests.Pieces
 {
     [TestFixture]
     public class BishopTests
-    { 
+    {
+        [Test]
+        public void BishopCanMoveForwardDiagonally()
+        {
+            var board = new Board();
+            var bishop = new Bishop(Player.Black);
+            board.AddPiece(Square.At(0, 0), bishop);
+            var moves = bishop.GetAvailableMoves(board).ToList();
+
+            moves.Count.Should().Be(7);
+            for (var i = 1; i < 8; i++)
+            {
+                moves.Should().Contain(Square.At(i, i));
+            }
+        }
     }
 }
