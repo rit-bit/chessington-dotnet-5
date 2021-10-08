0825dc15c11f0b48e3bf0d7306d1683f79bad6b2
Richard Sutch <richard.sutch@softwire.com>
2021-10-08 14:53:21 +0100

    Added test to check the bishop can move on all 4 diagonals

--- Chessington.GameEngine.Tests/Pieces/BishopTests.cs
+++ Chessington.GameEngine.Tests/Pieces/BishopTests.cs
@@ -9,7 +9,7 @@ namespace Chessington.GameEngine.Tests.Pieces
     public class BishopTests
     {
         [Test]
-        public void BishopCanMoveForwardDiagonally()
+        public void BishopCanMoveOn1Diagonal()
         {
             var board = new Board();
             var bishop = new Bishop(Player.Black);
@@ -21,6 +21,28 @@ namespace Chessington.GameEngine.Tests.Pieces
             {
                 moves.Should().Contain(Square.At(i, i));
             }
+        }
+        
+        [Test]
+        public void BishopCanMoveOn4Diagonals()
+        {
+            var board = new Board();
+            var bishop = new Bishop(Player.Black);
+            board.AddPiece(Square.At(4, 2), bishop);
+            var moves = bishop.GetAvailableMoves(board).ToList();
+
+            moves.Count.Should().Be(11);
+            moves.Should().Contain(Square.At(0, 6));
+            moves.Should().Contain(Square.At(1, 5));
+            moves.Should().Contain(Square.At(2, 0));
+            moves.Should().Contain(Square.At(2, 4));
+            moves.Should().Contain(Square.At(3, 1));
+            moves.Should().Contain(Square.At(3, 3));
+            moves.Should().Contain(Square.At(5, 1));
+            moves.Should().Contain(Square.At(5, 3));
+            moves.Should().Contain(Square.At(6, 0));
+            moves.Should().Contain(Square.At(6, 4));
+            moves.Should().Contain(Square.At(7, 5));
         }
     }
 }
