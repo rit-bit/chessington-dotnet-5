ca2eac5cdaea0eb7fbcd6843bccfe38ae3d5eadb
Richard Sutch <richard.sutch@softwire.com>
2021-10-11 09:35:30 +0100

    Added tests to check knight capturing and blocking

--- Chessington.GameEngine.Tests/Pieces/KnightTests.cs
+++ Chessington.GameEngine.Tests/Pieces/KnightTests.cs
@@ -42,5 +42,55 @@ namespace Chessington.GameEngine.Tests.Pieces
             moves.Should().Contain(Square.At(7, 4));
             moves.Should().Contain(Square.At(6, 3));
         }
+        
+        [Test]
+        public void KnightBlockedByFriendlyPieces()
+        {
+            var board = new Board();
+            var knight = new Knight(Player.White);
+            board.AddPiece(Square.At(5, 5), knight);
+            var friendlyPiece1 = new Pawn(Player.White);
+            board.AddPiece(Square.At(3, 4), friendlyPiece1);
+            var friendlyPiece2 = new Pawn(Player.White);
+            board.AddPiece(Square.At(5, 4), friendlyPiece2);
+            var moves = knight.GetAvailableMoves(board).ToList();
+            
+            moves.Count.Should().Be(7);
+            moves.Should().NotContain(Square.At(3, 4));
+            moves.Should().Contain(Square.At(4, 3));
+            
+            moves.Should().Contain(Square.At(3, 6));
+            moves.Should().Contain(Square.At(4, 7));
+            
+            moves.Should().Contain(Square.At(6, 7));
+            moves.Should().Contain(Square.At(7, 6));
+            
+            moves.Should().Contain(Square.At(7, 4));
+            moves.Should().Contain(Square.At(6, 3));
+        }
+        
+        [Test]
+        public void KnightCanCaptureOpposingPieces()
+        {
+            var board = new Board();
+            var knight = new Knight(Player.White);
+            board.AddPiece(Square.At(5, 5), knight);
+            var opposingPiece = new Pawn(Player.Black);
+            board.AddPiece(Square.At(3, 4), opposingPiece);
+            var moves = knight.GetAvailableMoves(board).ToList();
+            
+            moves.Count.Should().Be(8);
+            moves.Should().Contain(Square.At(3, 4));
+            moves.Should().Contain(Square.At(4, 3));
+            
+            moves.Should().Contain(Square.At(3, 6));
+            moves.Should().Contain(Square.At(4, 7));
+            
+            moves.Should().Contain(Square.At(6, 7));
+            moves.Should().Contain(Square.At(7, 6));
+            
+            moves.Should().Contain(Square.At(7, 4));
+            moves.Should().Contain(Square.At(6, 3));
+        }
     }
 }
